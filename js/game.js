447d302b508239ec1b71187531abe4ca11e9ca52
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2022-11-27 18:28:41 -0600

    Spawning brick grids for both players

--- js/game.js
+++ js/game.js
@@ -1,7 +1,5 @@
 // GAME CONCEPT: 2 player game of breakout: destory the other player's bricks!
 
-// TODO: game loop test
-// TODO: spawn bricks
 // TODO: control paddle (left + right)
 // TODO: spawn ball with random direction
 // TODO: wall collision (all walls are solid)
@@ -16,6 +14,13 @@
 
 const GAME_W = 240;
 const GAME_H = 320;
+
+// GRID PROPS
+const BRICK_W = 32;
+const BRICK_H = 16;
+const COLS = 6;
+const ROWS = 4;
+const PADDING = 4;
 
 // OBJECTS
 const PADDLE = {
@@ -39,26 +44,47 @@ const BALL = {
 const BRICK = {
   x: GAME_W / 2,
   y: 50,
-  w: 32,
-  h: 16,
+  w: BRICK_W,
+  h: BRICK_H,
   color: "red",
   speed: 0,
   type: "ball",
 };
 
-const GAME_OBJECTS = [PADDLE, BALL, BRICK];
+// UTILS
+const genGrid = (brick, rows, cols, start_x = 0, start_y = 0) => {
+  let new_grid = [];
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      // copy obj template
+      let new_brick = JSON.parse(JSON.stringify(brick));
+      console.log(new_brick);
+
+      // initial pos
+      new_brick.x = start_x + j * new_brick.w + PADDING * j;
+      new_brick.y = start_y + i * new_brick.h + PADDING * i;
+
+      // add to grid
+      new_grid.push(new_brick);
+    }
+  }
+  return new_grid;
+};
+
+const PLAYER_1_GRID = genGrid(BRICK, ROWS, COLS, BRICK_W / 2, BRICK_H);
+const PLAYER_2_GRID = genGrid(BRICK, ROWS, COLS, BRICK_W / 2, 224);
+
+const GAME_OBJECTS = [PADDLE, BALL, ...PLAYER_1_GRID, ...PLAYER_2_GRID];
 
 // LOOP
 const update = () => {};
 
 const draw = () => {
-  console.log("draw");
   context.fillStyle = "black";
   context.fillRect(0, 0, canvas.width, canvas.height);
 
   // render objects
   GAME_OBJECTS.forEach((obj) => {
-    console.log(obj);
     context.fillStyle = obj.color;
     context.fillRect(obj.x, obj.y, obj.w, obj.h);
   });
